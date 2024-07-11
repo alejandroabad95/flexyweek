cfa68ce311b68b58b8a07572250feccb55ce2a96
Alejandro Abad <alejandroabad95@gmail.com>
2024-07-11 11:10:34 +0200

    v0.4.0 redis funcionalidad completa

--- backend/main/tasks.py
+++ backend/main/tasks.py
@@ -4,14 +4,35 @@ from .models import Event
 from celery_config import app
 
 @app.task
+
 def unmark_completed_events():
-    # Obtener todos los eventos completados que deben desmarcarse
+    # Obtener todos los eventos completados
     events_to_unmark = Event.objects.filter(completed=True)
 
-    # Desmarcar los eventos completados
     for event in events_to_unmark:
-        event.completed = False
-        event.save()
+        # Obtener el tipo de actividad del evento
+        activity_type_id = event.activity.activity_type.id
 
+        if activity_type_id == 1:
+            # Si es un hábito, desmarcar el evento como completado
+            event.completed = False
+            event.save()
+            
+        elif activity_type_id == 2:
+            # Si es una tarea, eliminar el evento
+            event.delete()
+           
     # Información para registro o debugging
-    return f'Unmarked {len(events_to_unmark)} completed events.'
+    return f'Processed completed events.'
+
+# def unmark_completed_events():
+#     # Obtener todos los eventos completados que deben desmarcarse
+#     events_to_unmark = Event.objects.filter(completed=True)
+
+#     # Desmarcar los eventos completados
+#     for event in events_to_unmark:
+#         event.completed = False
+#         event.save()
+
+#     # Información para registro o debugging
+#     return f'Unmarked {len(events_to_unmark)} completed events.'
